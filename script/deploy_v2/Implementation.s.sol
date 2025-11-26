fa66679aaa13c16b098522fdac26780bd700cee2
Zayden Lee <zayden@to.nexus>
2025-11-26 18:21:45 +0900

    base bridge deplot

---
+++ script/deploy_v2/Implementation.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {BaseBridge} from "../../src/BaseBridge.sol";
+import {PriceFeed} from "../../src/PriceFeed.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+import {Const} from "../../src/lib/Const.sol";
+import {Script, console} from "forge-std/Script.sol";
+
+contract ImplementationScript is Script {
+    function setUp() public {}
+
+    function deployPriceFeedImpl() public {
+        vm.broadcast();
+        PriceFeed impl = new PriceFeed();
+        console.log("PriceFeed Implementation will be deployed to:", address(impl));
+    }
+
+    function deployBaseBridgeImpl() public {
+        vm.broadcast();
+        BaseBridge impl = new BaseBridge();
+        console.log("BaseBridge Implementation will be deployed to:", address(impl));
+    }
+}
