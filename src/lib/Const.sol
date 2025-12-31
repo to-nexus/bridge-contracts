119089a4bcbb72d67c77491dfbfe268ea9cd5107
Zayden Lee <zayden@to.nexus>
2025-12-31 19:55:04 +0900

    CBU-03-2

--- src/lib/Const.sol
+++ src/lib/Const.sol
@@ -21,7 +21,9 @@ library Const {
         MintFailed,
         TokenScoreOverflow,
         TokenCurrentVolumeOverflow,
-        CrossSupplyLimitExceeded
+        CrossSupplyLimitExceeded,
+        RemainingTransferFailed // extraCall 성공 후 잔액 환불 실패
+
     }
 
     /**
